331cb98be4543d84d0d9ef708e751babd64151e0
matheovll <113986545+matheovll@users.noreply.github.com>
2023-03-22 11:41:50 +0100

    Add files via upload

--- scripteRemplissage.py
+++ scripteRemplissage.py
@@ -2,27 +2,26 @@ import csv
 import unidecode
 
 # MATHEO VALLEE - 22/03/2023
-# Scripte de remplissage de table SQL
-# En donnant un fichier csv avec les données et le nom de la table en premiere ligne
-# Le scripte va creer un fichier SQL avec les requetes d'insertion des données 
-# (Fait pour Oracle et exeption gerer pour les apostrophes et les virgules dans les valeurs)
-# Gere aussi les dates (format dd-mm-yyyy) et les accents (avec la librairie unidecode) 
-# Ainsi que les VARCHAR
+# Create SQL table filling script from a CSV file
+# By giving a CSV file with the data and the table name in the first line
+# The script will create an SQL file with data insertion requests
+# Exception such as comma or apostrophe are managed
+# Also manage dates (DD-MM-Yyyy format) and accents (with the Unidecode bookstore) as well as the Varchar
 
 
-# Ouverture du fichier CSV
+# CSV file opening
 x = 0
-T=[] # Tableau qui contiendra les données du fichier CSV
-with open('Jour.csv', newline='') as csv_file: # Fichier source ||a modifier||
+T=[] # Table that will contain CSV file data
+with open('Jour.csv', newline='') as csv_file: # Source file || To modify ||
     reader = csv.reader(csv_file)
     for row in reader:
-        if (len(row) > 1): # Si la ligne contient plus d'une valeur
-            tmp = row[0] + "," + row[1] # On concatène les deux valeurs avec une virgule
+        if (len(row) > 1): # If the line contains more than one value
+            tmp = row[0] + "," + row[1] # We concaten the two values with a comma
         else :
-            tmp = row[0] # Sinon on prend la valeur seule
-        tmp = tmp.split(";") # On sépare les valeurs avec un point-virgule
-        T.append(tmp) # On ajoute la ligne au tableau
-        if (x == 1000000000): # Pour test on peux mettre que quelque lignes
+            tmp = row[0] # Otherwise we take the value alone
+        tmp = tmp.split(";") # We separate values with a semicolon
+        T.append(tmp) # We add the line to the board
+        if (x == 1000000000): # For test we can put only some lines
             break
         x = x + 1 
 
@@ -33,43 +32,43 @@ for i in T:
     print(i)
 '''
 
-#Creation du squellette de la requete SQL
+#Creation of the skeleton of the SQL request
 insertionSQL = 'INSERT INTO '
 insertionSQL += str(input("Nom de la table : ")) + ' VALUES ('
 
-# On demande le type de données de chaque colonne pour pouvoir l'adapté dans l'inssertion SQL
+# We ask for the type of data of each column to be able to adapt it in the SQL inssertion
 typeValeur = []
 for i in range(len(T[0])):
     typeValeur.append(str(input('\nSi type de ==> ' + T[0][i] +' :\n•VARCHAR -> 1\n•DATE -> 2\n•AUTRE -> 3\n')))
 
-# On supprime la première ligne du tableau qui contient les noms des colonnes
+# We delete the first line of the table which contains the names of the columns
 T.remove(T[0])
 
-# On crée le fichier SQL
-fichier = open('Remplissage_Quartier.sql', 'w') # Fichier de destination ||a modifier||
+# We create the SQL file
+fichier = open('Remplissage_Quartier.sql', 'w')# Destination file || To modify ||
 for i in T:
-    tmpInsertionSQL = insertionSQL # On crée une copie de la requete SQL
+    tmpInsertionSQL = insertionSQL # We create a copy of the SQL request
     tmp = ''
     x = 0
-    for j in i: # Pour chaque ligne dde la table
-        if (',' in j): # Si la valeur contient une virgule on la remplace par un point
+    for j in i: # For each line of the table
+        if (',' in j):# If the value contains a point we replace it with a point
             jBis = j.replace(',','.')
         else:
             jBis = j
-        if (typeValeur[x] == '1'): # Si le type de la valeur est VARCHAR
-            if ('\'' in jBis): # Si la valeur contient une apostrophe on la remplace par deux apostrophes
+        if (typeValeur[x] == '1'):# If the type of value is Varchar
+            if ('\'' in jBis):# If the value contains an apostrophe we replace it with two apostrophes
                 jBis = jBis.replace('\'','\'\'')
-            tmp +=str('\'' + unidecode.unidecode(jBis) + '\'')  # On ajoute la valeur à la requete SQL
-        elif (typeValeur[x] == '2'): # Si le type de la valeur est DATE
-            tmp += str('to_date(' + jBis + ',\'dd-mm-yyyy\')') # On ajoute la valeur à la requete SQL
+            tmp +=str('\'' + unidecode.unidecode(jBis) + '\'') # We add the value to the SQL request
+        elif (typeValeur[x] == '2'): # If the value type is date
+            tmp += str('to_date(' + jBis + ',\'dd-mm-yyyy\')')# We add the value to the SQL request
         else:
-            tmp +=str(jBis)  # Sinon on ajoute la valeur à la requete SQL
-        if (i.index(j) != len(i) - 1): # Si on est pas à la dernière valeur de la ligne on ajoute une virgule
+            tmp +=str(jBis) # Otherwise we add the value to the SQL request
+        if (i.index(j) != len(i) - 1):# If we are not at the last value of the line we add a comma
             tmp += ', '
         x = x + 1
     tmpInsertionSQL += tmp 
     tmpInsertionSQL += ');\n'
-    fichier.write(tmpInsertionSQL) # On écrit la requete SQL dans le fichier en ajoutant un retour à la ligne
+    fichier.write(tmpInsertionSQL) # We write the SQL request in the file by adding a return to the line
 fichier.close()
 
 
